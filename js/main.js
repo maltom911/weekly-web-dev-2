223c9084dfbddec75b4fa59b4c9933686b22beca
Tomek <maltom911@gmail.com>
2017-12-21 17:09:45 +0100

    responsywnosc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-$('.carousel').carousel({
-  interval: 500
-})
 
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
